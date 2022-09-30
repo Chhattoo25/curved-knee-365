1f8476809bc356de3954d875496e41cc7b420777
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-09-30 21:49:10 +0530

    fp01_172_day-04 day-02

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,23 @@ import "./App.css";
 import Navbar from "./Components/Header/Navbar";
 import SignIn from "./Components/SignIn";
 import { UserAuth } from "./Context/AuthContext";
+import AfterNavbar from "./Pages/AfterLogin/AfterNavbar";
 import PopularCompanies from "./Pages/CompanyReview/PopularCompanies";
 import Home from "./Pages/Home/Home";
 
 import { AllRoutes } from "./Pages/Routes/AllRoutes";
 
-import SalaryGuide from './Pages/SalaryGuide/SalaryGuide';
-
-
 function App() {
   const { user } = UserAuth();
-  return( 
+  return (
     <>
-  <div className="App">
-    {user?.displayName ? <Home /> : <SignIn />}</div>
+      <div className="App">{user?.displayName ? <Home /> : <SignIn />}</div>
 
-  <Navbar/>
-    <AllRoutes/>
-  <br/>
-  <br/>
-  <br/>
-      <SalaryGuide/>
+      <Navbar />
+
+      <AllRoutes />
     </>
-    );
-
+  );
 }
 
 export default App;
---
+++ src/Pages/AfterLogin/AfterLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AfterNavbar from './AfterNavbar'
+ 
+const AfterLogin = () => {
+  return (
+    <div>
+<AfterNavbar/>
+    </div>
+  )
+}
+
+export default AfterLogin
