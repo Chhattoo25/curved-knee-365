e8031f8943104af154fa7aa3910d81440d380f79
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-10-01 13:16:00 +0530

    day-5

---
+++ src/Pages/ApplyNow/Apply1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Apply1 = () => {
+  return (
+    <div>Apply1</div>
+  )
+}
+
+export default Apply1
--- src/Pages/FindJobs/FindJobs.jsx
+++ src/Pages/FindJobs/FindJobs.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import {BiSearch} from 'react-icons/bi'
-import {MdLocationPin} from 'react-icons/md'
-import { JobCard } from '../../Components/JobCard/JobCard'
-import { UserAuth } from '../../Context/AuthContext'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FullDetailedCard } from "../../Components/FullDetailedCard/FullDetailedCard";
+// import {BiSearch} from 'react-icons/bi'
+// import {MdLocationPin} from 'react-icons/md'
+import { JobCard } from "../../Components/JobCard/JobCard";
+import { SearchBar } from "../../Components/SearchBar/SearchBar";
+import { UserAuth } from "../../Context/AuthContext";
 
+const Home = () => {
+  // const [title, setTitle] = useState("");
+  // const [location, setLocation] = useState("");
 
-
-
-const FindJobs = () => {
-  const [title, setTitle] = useState("");
-  const [location, setLocation] = useState("");
-
-  const {user,logOut} = UserAuth();
+  const { user, logOut } = UserAuth();
 
   const handleSignOut = async () => {
     try {
-      await logOut()
+      await logOut();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  
+  };
 
   return (
     <>
-    <button onClick={handleSignOut}></button>
-    <h2>{user.displayName}</h2>
-    <COMPONENT>
-      <SEARCHBAR_CONTAINER>
+      <button onClick={handleSignOut}></button>
+      {/* <h2>{user.displayName}</h2> */}
+      <COMPONENT>
+        {/* <SEARCHBAR_CONTAINER>
         <SEARCHBAR_CONTAINER_WRAPPER>
           <h4>What</h4>
           <div><input type='text' placeholder='Job title, keywords, or company' value={title} onChange={(e)=>setTitle(e.target.value)} /> </div>
@@ -43,171 +41,202 @@ const FindJobs = () => {
           <button>Find jobs</button>
         </SEARCHBUTTON_WRAPPER>
     
-      </SEARCHBAR_CONTAINER>
+      </SEARCHBAR_CONTAINER> */}
+        <SearchBar />
 
-      <POST_RESUME_CONTAINER>
-        <div>
-          <a href="">Post your resume</a> <span>-It only takes a few seconds</span>
-        </div>
-        <div>
-          <a href="">Employers: Post a job</a> <span>- Your next hire is here</span>
-        </div>
-      </POST_RESUME_CONTAINER>
-      
-      <hr/>
-
-       <FeedBar>
-        <FeedBar_TABS>
+        <POST_RESUME_CONTAINER>
           <div>
-            <h3>Job feed <span>New</span></h3>
+            <a href="">Post your resume</a>{" "}
+            <span>-It only takes a few seconds</span>
           </div>
           <div>
-            <h3>Recent searches</h3>
+            <a href="">Employers: Post a job</a>{" "}
+            <span>- Your next hire is here</span>
           </div>
-        </FeedBar_TABS>
-        <div></div>
-       </FeedBar>
-      
-      <JobCard></JobCard>
-    </COMPONENT>
+        </POST_RESUME_CONTAINER>
+
+        <hr />
+
+        <FeedBar>
+          <FeedBar_TABS>
+            <div>
+              <h3>
+                Job feed <span>New</span>
+              </h3>
+            </div>
+            <div>
+              <h3>Recent searches</h3>
+            </div>
+          </FeedBar_TABS>
+          <div></div>
+        </FeedBar>
+        <CARDS_SECTION>
+          <JOBCARDS_CONTAINER>
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+          </JOBCARDS_CONTAINER>
+
+          <FULLJOBCARD_CONTAINER>
+            <FullDetailedCard />
+          </FULLJOBCARD_CONTAINER>
+
+        </CARDS_SECTION>
+      </COMPONENT>
     </>
+  );
+};
 
-  )
-  
-}
-
-export default FindJobs
-
-const COMPONENT = styled.div`
-border: 1px solid red;
- display:flex;
- flex-direction: column;
- width: 100%;
- justify-content: center;
- align-items: center;
- margin: auto;
- 
- hr{
-  width: 100%;
- }
-`
-const SEARCHBAR_CONTAINER = styled.div`
- display: flex;
- gap: 1.5rem;
- justify-content: center;
- align-items: center;
-
- @media screen and (min-width: 400px) and (max-width: 768px){
-  flex-direction: column;
- }
-`
-const SEARCHBAR_CONTAINER_WRAPPER = styled.div`
-border: 1px solid black;
-display: flex;
-justify-content: space-around;
-align-items: center;
-border-radius: 0.5rem;
-width: 22rem;
-height: 2.5rem;
-padding: 0 0.5rem;
-h4{
-  font-weight: bold;
-}
-div{
-  display: flex; 
-  input{
-    width: 14rem;
-    border-style: hidden;
-    font-size: 14px;
-    &:focus {
-      outline: none;
-    }
-  }
-  button{
-    border:none;
-    font-weight: 500;
-    background-color: none;
-    font-size: 16px;
-
-    @media screen and (min-width: 400px) and (max-width: 768px){
-     width: 90%;
-     color: red;
-    }
-  }
-}
-
-
-`
+export default Home;
 // @media screen and (min-width: 400px) and (max-width: 768px){
-  
+
 // }
-const SEARCHBUTTON_WRAPPER = styled.div`
- button{
-   padding: 0.75rem 1rem;
-   background-color: #2557a7;
-   border: 1px solid transparent;
-   border-radius: 0.5rem;
-   color: white;
-   font-weight: 700;
- }
-`
-const POST_RESUME_CONTAINER = styled.div`
- padding: 1.5rem;
- div{
+const COMPONENT = styled.div`
+  border: 1px solid red;
   display: flex;
+  flex-direction: column;
+  width: 100%;
   justify-content: center;
-  padding: 0.5rem 0;
-  a{
-    text-decoration: none;
-    color: #2557a7;
-    font-size: 16px;
-    font-weight: bold;
+  align-items: center;
+  margin: auto;
+
+  hr {
+    width: 100%;
   }
- }
-`
+`;
+// const SEARCHBAR_CONTAINER = styled.div`
+//  display: flex;
+//  gap: 1.5rem;
+//  justify-content: center;
+//  align-items: center;
+
+//  @media screen and (min-width: 400px) and (max-width: 768px){
+//   flex-direction: column;
+//  }
+// `
+// const SEARCHBAR_CONTAINER_WRAPPER = styled.div`
+// border: 1px solid black;
+// display: flex;
+// align-items: center;
+// border-radius: 0.5rem;
+// width: 22rem;
+// height: 2.5rem;
+// padding: 0 0.5rem;
+// h4{
+//   font-weight: bold;
+//   margin-right: 15px;
+// }
+// div{
+//   display: flex;
+//   input{
+//     width: 14rem;
+//     border-style: hidden;
+//     font-size: 14px;
+//     &:focus {
+//       outline: none;
+//     }
+//     @media screen and (min-width: 400px) and (max-width: 768px){
+//       width: 30rem;
+//      }
+//   }
+// }
+// @media screen and (min-width: 400px) and (max-width: 768px){
+//   width: 35rem;
+//  }
+
+// `
+// const SEARCHBUTTON_WRAPPER = styled.div`
+//  button{
+//   align-self: end;
+//    padding: 0.75rem 1rem;
+//    background-color: #2557a7;
+//    border: 1px solid transparent;
+//    border-radius: 0.5rem;
+//    color: white;
+//    font-weight: 700;
+//    @media screen and (min-width: 400px) and (max-width: 768px){
+//     padding: 0.75rem 16rem;
+//    }
+//  }
+// `
+const POST_RESUME_CONTAINER = styled.div`
+  padding: 1.5rem;
+  div {
+    display: flex;
+    justify-content: center;
+    padding: 0.5rem 0;
+    a {
+      text-decoration: none;
+      color: #2557a7;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+`;
 
 //Jobfeed-Bar
 const FeedBar = styled.div`
- display: flex;
- width: 100%;
- flex-direction: column;
- justify-content: center;
- align-items: center;
- 
-`
-const FeedBar_TABS= styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-div{
-
-  width: 17rem;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const FeedBar_TABS = styled.div`
   display: flex;
   justify-content: center;
-  border-bottom: 5px solid #2557a7;
-  h3{
-    display: block;
-    span{
-      padding: 0.5rem;
-      background-color: black;
-      color: white;
-      margin-left: 0.25rem;
-      font-weight: 400;
-      border-radius: 0.75rem;
+  align-items: center;
+  div {
+    width: 17rem;
+    display: flex;
+    justify-content: center;
+    border-bottom: 5px solid #2557a7;
+    h3 {
+      display: block;
+      span {
+        padding: 0.5rem;
+        background-color: black;
+        color: white;
+        margin-left: 0.25rem;
+        font-weight: 400;
+        border-radius: 0.75rem;
+      }
     }
   }
-}
+`;
 
+const CARDS_SECTION = styled.div`
+  display: flex;
+  gap: 1rem;
+  width: 60%;
+  justify-content: center;
+  position: relative;
+`;
+const JOBCARDS_CONTAINER = styled.div`
+ display: flex;
+ flex-direction: column;
+ border: 3px dotted red;
+ justify-content: center;
+ align-items: center;
+ gap: 1rem;
+ padding: 0 1rem;
+ width: 42%;
+ @media screen and (min-width: 769px) and (max-width: 1024px){
+  width: 30rem;
+ }
+ @media screen and (min-width: 400px) and (max-width: 768px){
+  width: 100%;
+ }
 `
+const FULLJOBCARD_CONTAINER = styled.div`
+ display: flex;
+ border: 3px solid teal;
+ width: 50%;
+ height: 100vh;
+ position: sticky;
 
-{/* <FeedBar>
-  <FeedBar_TABS>
-    <div>
-      <h3>Job feed</h3>
-      <span>New</span>
-    </div>
-    <div>
-      <h3>Recent searches</h3>
-    </div>
-  </FeedBar_TABS>
-</FeedBar> */}
+ @media screen and (min-width: 400px) and (max-width: 768px){
+  display: none;
+ }
+`
